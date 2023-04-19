39f271073163bcbfb2116401a793cc823c0e1ad7
otunba32 <emmanuel.abia32@gmail.com>
2023-04-19 14:59:11 -0700

    Wqtask4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- creates a database hbnb_test_db
+-- creates a new user hbnb_test
+-- The user's password is hbnb_test_pwd
+-- User should have all priviledges
+-- hbnb_test should have SELECT priviledge on performance_schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
